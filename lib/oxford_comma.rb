114e19d9e4b93bbdeb59f762127042d3bfd25410
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-15 01:48:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  #puts array[0,array.length-1]
+  puts array.length
+  if array.length == 1
+    return array[0]
+  end
+  listString=array[0,array.length-1].join(", ")
+  listString+=" and #{array.last}"
 
+
+#  ListString=""
+#  array.reverse
+#  array.each{|x,i|
+#  if  }
 end
+
+#puts oxford_comma(["Alex","emily","Nate"])
+#puts oxford_comma(["Alex"])
+#
